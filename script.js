f0dc2edeaa41be08fb283d8c56316317875236e2
Bernardo <bernardocardosodacosta@gmail.com>
2021-09-08 20:18:04 -0300

    Modificações do DOM no CSS.

--- script.js
+++ script.js
@@ -7,10 +7,13 @@ let yumeko = ""
 let voce = ""
 let resultado = ""
 
-escolhas.forEach(escolha => escolha.addEventListener("click", () => {
+escolhas.forEach(escolha => escolha.addEventListener("click", (e) => {
     voce = e.target.id 
-    escolhaTua.innerHTML = " " + você
-}) )
+    escolhaTua.innerHTML = " " + voce
+    yumekoGambling()
+    gambler()
+    display()
+}))
 
 const yumekoGambling = () => {
     const random = Math.floor(Math.random(1) * 3) + 1
@@ -30,24 +33,49 @@ const yumekoGambling = () => {
 const gambler = () => {
     if (yumeko === voce) {
         resultado = "Ninguém...da próxima vez tente contra a Mary Saotome" 
+        let mary = document.createElement("img")
+        mary.src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fnyaa.shikimori.one%2Fsystem%2Fuser_images%2Foriginal%2F644950%2F993165.jpg&f=1&nofb=1"
+        document.querySelector("#last").appendChild(mary)
     }
     if (yumeko === "pedra" && voce === "papel") {
         resultado = "Você. O que acha de jogar contra a Itsuki Sumeragi?" 
+        let iti = document.createElement("img")
+        iti.src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F2e%2F07%2F2a%2F2e072acc0157c122dce090c594b69e26.jpg&f=1&nofb=1"
+        document.querySelector("#last").appendChild(iti)
     }
     if (yumeko === "pedra" && voce === "papel") {
         resultado = "Você perdeu...Já pegou seu novo crachá?"
+        let cracha = document.createElement("img")
+        cracha.src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fkakeguruimerch.com%2Fwp-content%2Fuploads%2F2021%2F01%2FAnime-Kakegurui-Compulsive-Gambler-Necklace-Jabami-Yumeko-ID-Card-Acrylic-Pendant-Necklaces-Cosplay-Jewelry-5-600x600.jpg&f=1&nofb=1"
+        document.querySelector("#last").appendChild(cracha)
     }
     if (yumeko === "papel" && voce === "tesoura") {
         resultado = "Você. O que acha de jogar contra a Itsuki Sumeragi?"
+        let iti = document.createElement("img")
+        iti.src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F2e%2F07%2F2a%2F2e072acc0157c122dce090c594b69e26.jpg&f=1&nofb=1"
+        document.querySelector("#last").appendChild(iti)
     }
     if (yumeko === "papel" && voce === "pedra") {
         resultado = "Você perdeu...Já pegou seu novo crachá?"
+        let cracha = document.createElement("img")
+        cracha.src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fkakeguruimerch.com%2Fwp-content%2Fuploads%2F2021%2F01%2FAnime-Kakegurui-Compulsive-Gambler-Necklace-Jabami-Yumeko-ID-Card-Acrylic-Pendant-Necklaces-Cosplay-Jewelry-5-600x600.jpg&f=1&nofb=1"
+        document.querySelector("#last").appendChild(cracha)
     }
     if (yumeko === "tesoura" && voce === "pedra") {
         resultado = "Você. O que acha de jogar contra a Itsuki Sumeragi?"
+        let iti = document.createElement("img")
+        iti.src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F2e%2F07%2F2a%2F2e072acc0157c122dce090c594b69e26.jpg&f=1&nofb=1"
+        document.querySelector("#last").appendChild(iti)
     }
     if (yumeko === "tesoura" && voce === "papel") {
         resultado = "Você perdeu...Já pegou seu novo crachá?"
+        let cracha = document.createElement("img")
+        cracha.src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fkakeguruimerch.com%2Fwp-content%2Fuploads%2F2021%2F01%2FAnime-Kakegurui-Compulsive-Gambler-Necklace-Jabami-Yumeko-ID-Card-Acrylic-Pendant-Necklaces-Cosplay-Jewelry-5-600x600.jpg&f=1&nofb=1"
+        document.querySelector("#last").appendChild(cracha)
     }
     resultadoTela.innerHTML = " " + resultado
+}
+const display = () => {
+    let final = document.querySelector("#bottom")
+    final.style.display = "block"
 }
